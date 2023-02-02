c1af9c2e44cf786389b2e947026fcba35e2763dd
Airly12000 <littlefrancis28@gmail.com>
2023-02-02 17:24:49 +0000

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,15 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Header from '../components/Header';
+import { sanityClient, urlFor } from '../sanity';
+import { Post } from '../typings';
+import Link from 'next/link';
 
-const Home: NextPage = () => {
+interface Props {
+	posts: Post[];
+}
+
+const Home: NextPage<Props> = ({ posts }) => {
 	return (
 		<div className='max-w-7xl mx-auto'>
 			<Head>
@@ -33,8 +40,57 @@ const Home: NextPage = () => {
 				/>
 			</div>
 			{/* Posts */}
+			<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
+				{posts.map((post) => (
+					<Link key={post._id} href={`/post/${post.slug.current}`}>
+						<div className='border rounded-lg group cursor-pointer overflow-hidden'>
+							<img
+								className='w-full object-cover h-60 group-hover:scale-105 transition-transform duration-200 ease-in-out'
+								src={urlFor(post.mainImage).url()!}
+								alt=''
+							/>
+							<div className='flex justify-between p-5 bg-white'>
+								<div>
+									<p className='text-lg font-bold'>{post.title}</p>
+									<p className='text-xs'>
+										{post.description} by {post.author.name}
+									</p>
+								</div>
+								<img
+									className='object-contain w-12 h-12 rounded-full'
+									src={urlFor(post.author.image).url()!}
+									alt=''
+								/>
+							</div>
+						</div>
+					</Link>
+				))}
+			</div>
 		</div>
 	);
+};
+
+export const getServerSideProps = async () => {
+	const query = `
+	*[_type == "post"]{
+		_id,
+		title,
+		author -> {
+			name,
+			image
+		},
+		description,
+		mainImage,
+		slug
+	}`;
+
+	const posts = await sanityClient.fetch(query);
+
+	return {
+		props: {
+			posts,
+		},
+	};
 };
 
 export default Home;
